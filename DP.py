15c060fb16e296c7fa4efd28df8c7f9ac35e6e3a
alex <alexmiller.ml@gmail.com>
2015-09-24 22:50:15 -0700

    knapsack

--- DP.py
+++ DP.py
@@ -64,7 +64,56 @@ def lcs(a, b):
 
     return num, path
 
+# knapsack 0/1
+def knapsack(items, s, getpath=True):
+    '''
+    0/1 knapsack problem
+    params: 
+    items contains triples of the form [name, weight, value]
+    s is the maximum weight
+    '''
+    
+    n = len(items)
+    # we define a grid with each row representing a number of items
+    # and each column representing a different weight sum
+    # so (i-1, j-1) := max value obtained by using i items to make maximum j weight
 
+    grid = [[0 for j in range(s+1)] for i in range(n+1)]
+    
+    for i in range(1, n+1):
+        for j in range(1, s+1):
+            
+            # by default, pull the value from above
+            grid[i][j] = grid[i-1][j]
+            
+            # if the weight of one of the allowable items is less than
+            # the max weight, consider adding it
+            if items[i-1][1] <= j:
+                
+                # in this case we must also consider the residual
+                residual_weight = j - items[i-1][1]
+                possible_val = items[i-1][2] + grid[i-1][residual_weight]
+                grid[i][j] = max(grid[i][j], possible_val)
+    
+    # save optimal value
+    maxval = grid[n][s]
+    
+    if getpath:
+        
+        # backtrack through the grid
+        path = []
+        while s > 0 and n > 0:
+            
+            # if we didn't inherit from above, we must have taken the item
+            if grid[n][s] != grid[n-1][s]:
+                path.append(n)
+                wt = items[n-1][1]
+                n -= 1; s -= wt
+            else:
+                n -= 1
+  
+    return maxval, path
+            
 def bridges(lst):
 
     pass
